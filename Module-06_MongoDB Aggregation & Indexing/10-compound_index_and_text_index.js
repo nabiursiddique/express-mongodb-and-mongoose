716ced7888330f581b7b0ab9daec5bbc0227aaa0
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-13 21:45:58 +0600

    compound index and text index learned

--- Module-06_MongoDB Aggregation & Indexing/10-compound_index_and_text_index.js
+++ Module-06_MongoDB Aggregation & Indexing/10-compound_index_and_text_index.js
@@ -1,1 +1,6 @@
 //*** Explore Compound Index And Text Index 
+
+// text indexing
+db.getCollection("massive-data").createIndex({ about: "text" })
+db.getCollection("massive-data").find({ $text: { $search: "dolor" } })
+
