1818499ed1b897bbb26fdd9e8e284d5c251ae4fe
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-14 22:06:53 +0600

    learned stream and buffer

---
+++ Module-07_Foundation of Express/learning-node/stream-buffer.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const { dirname } = require('path');
+
+// creating a server using raw nodejs
+const server = http.createServer();
+
+// server listener
+server.on('request', (req, res) => {
+    if (req.url === '/read-file' && req.method === 'GET');
+
+    // streaming file reading
+    const readableStream = fs.createReadStream(__dirname + '/texts/read.txt')
+    readableStream.on('data', (buffer) => {
+        res.write(buffer);
+    })
+    readableStream.on("end", () => {
+        res.end("Hello from world!")
+    })
+
+})
+
+server.listen(5000, () => {
+    console.log(`server is listening on port 5000`);
+})
