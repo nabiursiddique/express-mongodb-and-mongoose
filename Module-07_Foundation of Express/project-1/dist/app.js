774c15ec58c1c8e22dca76b26fc0f2f94de94747
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-15 20:03:35 +0600

    learned creating and using router in expressjs

--- Module-07_Foundation of Express/project-1/dist/app.js
+++ Module-07_Foundation of Express/project-1/dist/app.js
@@ -5,10 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// creating and using router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "course created successfully",
+        data: course,
+    });
+});
 // middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
--- Module-07_Foundation of Express/project-1/src/app.ts
+++ Module-07_Foundation of Express/project-1/src/app.ts
@@ -1,10 +1,36 @@
 import express, { NextFunction, Request, Response } from "express";
 const app = express();
-const port = 3000;
 
 // parsers
 app.use(express.json());
 app.use(express.text());
+
+// creating and using router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course created successfully",
+    data: course,
+  });
+});
 
 // middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
