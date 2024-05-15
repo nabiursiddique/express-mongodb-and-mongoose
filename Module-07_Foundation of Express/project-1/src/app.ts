4db33fc4fbf3da30ebb1b2e85fef34e1ab3f6933
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-15 08:00:26 +0600

    learning expressjs with typescript

---
+++ Module-07_Foundation of Express/project-1/src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+// parsers
+app.use(express.json());
+app.use(express.text());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello developers");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "successfully received data",
+  });
+});
+
+export default app;
---
+++ Module-07_Foundation of Express/project-1/src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import { Server } from "http";
+
+const PORT = 5000;
+
+let server: Server;
+
+async function bootstrap() {
+  server = app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+}
+
+bootstrap();
