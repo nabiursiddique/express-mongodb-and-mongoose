45a0b4461dc4a013423c555432234e331240e1c9
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-17 12:58:34 +0600

    project is setting up

--- Module-08_mastering_the_core_concepts_of_mongoose/first-project/src/app.ts
+++ Module-08_mastering_the_core_concepts_of_mongoose/first-project/src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 
@@ -7,7 +7,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 });
